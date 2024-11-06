0d9ec5a1f85a62302d2eea531aa7262d58f7723e
Ephraim Ehima <ehima87@gmail.com>
2024-11-06 14:34:17 +0100

    ephraimhtml

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const express = require('express');
 const app = express();
 const port = 3030;
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get("/ephraim", (req, res) => {
+    res.sendFile(__dirname + '/ephraim.html');
+});
+
 // listening on port 3030
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`🚀🔥Server is running on port http://localhost:${port}`);
 });
