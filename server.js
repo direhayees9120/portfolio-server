d81c7e966b6702976170d44568a3294bce4dc971
tizbami <aurrietadah@gmail.com>
2024-11-06 15:06:23 +0100

    done all

--- server.js
+++ server.js
@@ -5,7 +5,20 @@ const express = require('express');
 const app = express();
 const port = 3030;
 
+
+// define a routes
+app.get("/bami", (req, res) => {
+  res.sendFile(__dirname + "/bami.html");
+});
+
 // listening on port 3030
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port http://localhost:${port}`);
 });
+
+
+
+
+
+
+
